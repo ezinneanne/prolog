fbd586d0db50a4a239500f8a9e71460a996bbc8a
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-07-31 12:22:06 +0100

    first commit

---
+++ cheetah.pl
@@ -0,0 +1,20 @@
+% Facts
+spot_pattern(cheetah, spotted).
+body_size(cheetah, medium).
+leg_length(cheetah, long).
+tail_length(cheetah, long).
+habitat(cheetah, grassland).
+habitat(cheetah, savanna).
+diet(cheetah, carnivore).
+speed(cheetah, fast).
+
+% Rules
+is_cheetah(Animal) :-
+    spot_pattern(Animal, spotted),
+    body_size(Animal, medium),
+    leg_length(Animal, long),
+    tail_length(Animal, long),
+    habitat(Animal, grassland),
+    habitat(Animal, savanna),
+    diet(Animal, carnivore),
+    speed(Animal, fast).
