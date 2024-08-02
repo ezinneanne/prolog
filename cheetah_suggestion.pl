8e68d3b3758a671ab32d83a92b47d9da0b42db85
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-08-02 15:02:19 +0100

    first commit

--- cheetah_suggestion.pl
+++ cheetah_suggestion.pl
@@ -28,104 +28,56 @@ start :-
 % ?- start.
 
 
-Knowledgebase for cheetah; fast, spotted, slender bodies, small heads, tear-like facial streaks
-Questions to ask the user: is the animal fast? Is the animal spotted, does it have a slender body, does it have a small head, does it have tear-like facial streaks
+% Knowledgebase for cheetah; fast, spotted, slender bodies, small heads, tear-like facial streaks
+% Questions to ask the user: is the animal fast? Is the animal spotted, does it have a slender body, does it have a small head, does it have tear-like facial streaks
 
-What is the speed range of the animal (fast F or slow S)? yes
-Is the animal S spotted or plain P? yes
-Is the animal S slender or C chubby? yes
-Does it have a S small head or B big head? yes
-Does it have T tear-like facial streaks or Not N? Yes
+% What is the speed range of the animal (fast F or slow S)? yes
+% Is the animal S spotted or plain P? yes
+% Is the animal S slender or C chubby? yes
+% Does it have a S small head or B big head? yes
+% Does it have T tear-like facial streaks or Not N? Yes
 
-It is definitely a cheetah
-If yes&yes&yes&yes
-
-
-Type
-Boolean
-Text
-FSSST
-True
-It is definitely a cheetah
-FSSSN
-False
-It is not a cheetah as tear-like facial streaks are unique to only cheetahs
-SPCBT
-True
-It is possibly a cheetah as cheetahs have unique tear-like facial streaks
-FPSBT
-True
-It is possibly a cheetah as cheetahs have unique tear-like facial streaks
-SPCSN
-False
-It is not a cheetah
-SPCBN
-False
-It is not a cheetah
-SSSSN
-False
-It is not a cheetah
-SPCSN
-False
-It is not a cheetah
+% It is definitely a cheetah
+% If yes&yes&yes&yes
 
 
+% Type
+% Boolean
+% Text FSSST True It is definitely a cheetah FSSSN False It is not a cheetah as tear-like facial streaks are unique to only cheetahs SPCBT
+% True It is possibly a cheetah as cheetahs have unique tear-like facial streaks FPSBT True It is possibly a cheetah as cheetahs have unique tear-like facial streaks
+% SPCSN False It is not a cheetah SPCBN False It is not a cheetah SSSSN False It is not a cheetah SPCSN False It is not a cheetah
 
 
+% What is the speed range of the animal (fast or slow)? yes
+% Is the animal spotted or plain? yes
+% Is the animal slender or chubby? yes
+% Does it have a small head or big head? yes
+% Does it have tear-like facial streaks? No
+
+% It is not a cheetah as tear-like facial streaks are unique to only cheetahs If yes&yes&yes&no = false
+
+% What is the speed range of the animal (fast or slow)? Slow | No
+% Is the animal spotted or plain? Plain| No
+% Is the animal slender or chubby? Chubby|No
+% Does it have a small head or big head? Big head|No
+% Does it have tear-like facial streaks? Yes
+
+% It is possibly a cheetah as cheetahs have unique tear-like facial streaks
 
 
+% What is the speed range of the animal (fast or slow)? yes
+% Is the animal spotted or plain? No
+% Is the animal slender or chubby? yes
+% Does it have a small head or big head? No
+% Does it have tear-like facial streaks? Yes
+
+% It is possibly a cheetah as cheetahs have unique tear-like facial streaks
 
 
+% What is the speed range of the animal (fast or slow)? No
+% Is the animal spotted or plain? yes
+% Is the animal slender or chubby? No
+% Does it have a small head or big head? Yes
+% Does it have tear-like facial streaks? No
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-song('https://www.youtube.com/watch?v=c8YIlU_30Kk',jazz,M,P):- M = happy ,(P= (entj) ; P=(enfj) ; P=(enfp)),!.
-
-Suggest(‘It is definitely a cheetah’, T):- (T= FSSST),!.
-
-What is the speed range of the animal (fast or slow)? yes
-Is the animal spotted or plain? yes
-Is the animal slender or chubby? yes
-Does it have a small head or big head? yes
-Does it have tear-like facial streaks? No
-
-It is not a cheetah as tear-like facial streaks are unique to only cheetahs
-
-If yes&yes&yes&no = false
-
-What is the speed range of the animal (fast or slow)? Slow | No
-Is the animal spotted or plain? Plain| No
-Is the animal slender or chubby? Chubby|No
-Does it have a small head or big head? Big head|No
-Does it have tear-like facial streaks? Yes
-
-It is possibly a cheetah as cheetahs have unique tear-like facial streaks
-
-
-What is the speed range of the animal (fast or slow)? yes
-Is the animal spotted or plain? No
-Is the animal slender or chubby? yes
-Does it have a small head or big head? No
-Does it have tear-like facial streaks? Yes
-
-It is possibly a cheetah as cheetahs have unique tear-like facial streaks
-
-
-What is the speed range of the animal (fast or slow)? No
-Is the animal spotted or plain? yes
-Is the animal slender or chubby? No
-Does it have a small head or big head? Yes
-Does it have tear-like facial streaks? No
-
-It is not a cheetah
+% It is not a cheetah
